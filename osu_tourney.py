2d7e807ce6e59b0168af3bd2474f0e5732494389
Aaron Liao <aliao4@uci.edu>
2018-01-14 14:40:06 -0800

    transposed canvas and widgets

--- osu_tourney.py
+++ osu_tourney.py
@@ -8,25 +8,25 @@ class Gui():
         self.root=root
         self.entry = tk.Entry(root)
         stvar=tk.StringVar()
-        stvar.set("one")
+        stvar.set("Player 1")
 
-        self.canvas=tk.Canvas(root, width=300, height=200, background='white')
-        self.canvas.grid(row=0,column=1)
+        self.canvas=tk.Canvas(root, width=1280, height=720, background='white')
+        self.canvas.grid(row=0,column=0)
 
         frame = Frame(self.root)
-        frame.grid(row=0,column=0, sticky="n")
+        frame.grid(row=1,column=0, sticky="n")
 
-        self.option=tk.OptionMenu(frame, stvar, "one", "two", "three")
-        label1=Label(frame, text="Figure").grid(row=0,column=0, sticky="nw")
-        label2=Label(frame, text="X").grid(row=1,column=0, sticky="w")
-        label3=Label(frame, text="Y").grid(row=2,column=0, sticky="w")
-        self.option.grid(row=0,column=1,sticky="nwe")
-        entry = Entry(frame).grid(row = 1,column = 1,sticky = E+ W)
-        entry1 = Entry(frame).grid(row = 2,column = 1, sticky = E)
-        Button1=Button(frame,text="Draw").grid(row = 3,column = 1, sticky = "we")
+        self.option=tk.OptionMenu(frame, stvar, "Player 1", "Player 2")
+        label1=Label(frame, text="Figure").grid(row=1,column=0, sticky="nw")
+        label2=Label(frame, text="X").grid(row=2,column=0, sticky="w")
+        label3=Label(frame, text="Y").grid(row=3,column=0, sticky="w")
+        self.option.grid(row=1,column=1,sticky="nwe")
+        entry = Entry(frame).grid(row = 2,column = 1,sticky = E+ W)
+        entry1 = Entry(frame).grid(row = 3,column = 1, sticky = E)
+        Button1=Button(frame,text="Draw").grid(row = 4,column = 1, sticky = "we")
         figure1=self.canvas.create_rectangle(80, 80, 120, 120, fill="blue")
 
-        #Grid.columnconfigure(self.root,1,weight=1, size=200)
+        # Grid.rowconfigure(self.root,0,weight=720)
 
     def create_preview(self):
         self.canvas = tk.Canvas(self.master, width = 1280, height = 720,
